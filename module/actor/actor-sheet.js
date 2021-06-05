0ef3aafba48dbbe1736e3b235b65ce7e39bdf83f
TheLastScrub <67403321+TheLastScrub@users.noreply.github.com>
2021-06-05 15:04:18 -0700

    default bond item image to mystery person

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -294,7 +294,10 @@ export class DeltaGreenActorSheet extends ActorSheet {
     }
     else if(type == "bond"){
       itemData.data.score = this.object.data.data.statistics.cha.value; // Can vary, but at character creation starting bond score is usually agent's charisma
+      itemData.img = "icons/svg/mystery-man.svg"
     }
+
+    console.log(itemData);
     
     // Finally, create the item!
     //return this.actor.createOwnedItem(itemData);
